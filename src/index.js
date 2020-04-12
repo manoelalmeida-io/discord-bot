0bb16eeaab225bf4933ebed78c876f1e93ee1d11
Manoel Almeida <manoel.almeida@bandtec.com.br>
2020-04-11 21:18:51 -0300

    Example youtube broadcast

--- src/index.js
+++ src/index.js
@@ -1,4 +1,6 @@
 const Discord = require('discord.js');
+const ytdl = require('ytdl-core');
+
 const client = new Discord.Client();
 
 const { token } = require('./config/auth.json');
@@ -7,9 +9,13 @@ client.on('ready', () => {
   console.log(`Logged in as ${client.user.tag}!`);
 });
 
-client.on('message', msg => {
+client.on('message', async msg => {
   if (msg.content === 'ping') {
     msg.reply('Pong!');
+  }
+  if (msg.content === 'p') {
+    const broadcast = await msg.member.voice.channel.join();
+    broadcast.play(ytdl('https://www.youtube.com/watch?v=SCxBtaZERR4', { type: 'opus', filter: 'audioonly' }))
   }
 });
 
