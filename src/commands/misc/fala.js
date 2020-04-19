b18e02883a7d93e6bd492bb18f99749720eae29d
reisrb <rafael.reis@bandtec.com.br>
2020-04-19 01:27:55 -0300

    removing audio from youtube from class "fala"

--- src/commands/misc/fala.js
+++ src/commands/misc/fala.js
@@ -3,15 +3,9 @@ const ytdl = require('ytdl-core');
 const fala = async ({ msg }) => {
 
   msg.channel.send(
-    `Fala, ${msg.author}. Bom dia, cara.`, {tts: true}
+    `Fala ${msg.author} Bom dia cara`, {tts: true}
   ); 
 
-  const url = 'https://www.youtube.com/watch?v=3h52KJ10Bxw';
-
-  const connection = await msg.member.voice.channel.join(); 
-  await connection.play(ytdl(url, { filter: 'audioonly' }));
-
-  console.log('playing now:', url);
 }
 
 module.exports = fala;
