67b98bdbfdda5088e70f3cf5be6e068b67eed163
reis <rafael.reis@bandtec.com.br>
2020-05-05 00:04:31 -0300

    next music once dispatcher finish

--- src/commands/music/np.js
+++ src/commands/music/np.js
@@ -5,7 +5,7 @@ const queue = require('../../components/music/queue');
 const np = async ({ msg }) => {
   const connection = await msg.member.voice.channel.join();
 
-  videoInfo = await ytdl.getBasicInfo(queue.playingNow());
+  videoInfo = await ytdl.getBasicInfo(queue.playingNow().args[0]);
 
   msg.channel.send({ embed: embed(videoInfo.title, msg.author) });
 }
--- src/commands/music/play.js
+++ src/commands/music/play.js
@@ -6,16 +6,34 @@ const play = async ({ msg, args }) => {
   const connection = await msg.member.voice.channel.join();
 
   if (queue.isEmpty()) {
-    await connection.play(ytdl(args[0], { filter: 'audioonly' }));
+    let dispatcher = await connection.play(ytdl(args[0], { filter: 'audioonly' }));
+
+    dispatcher.once('finish', () => {
+      queue.shift();
+      if (!queue.isEmpty()) {
+        next(queue.playingNow());
+      }
+    });
   }
 
   videoInfo = await ytdl.getBasicInfo(args[0]);
 
   msg.channel.send({ embed: embed(videoInfo.title, msg.author, queue.isEmpty()) });
-  queue.add(args[0]);
+  queue.add({ msg, args });
 
   console.log('playing now:', args);
-  console.log(queue);
+}
+
+const next = async({ msg, args }) => {
+  const connection = await msg.member.voice.channel.join();
+  let dispatcher = await connection.play(ytdl(args[0], { filter: 'audioonly' }));
+
+  dispatcher.once('finish', () => {
+    queue.shift();
+    if (!queue.isEmpty()) {
+      next(queue.playingNow());
+    }
+  });
 }
 
 module.exports = play;
